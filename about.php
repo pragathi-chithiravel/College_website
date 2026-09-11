1b5b443cd457971534f6c5dfeed62b74f2ade51f
pragathi-chithiravel <pragathivel05@gmail.com>
2026-09-11 13:46:22 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="logo.jpg">
+    <title>Meenakshi Chandrasekaran College</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- <link rel="stylesheet" href="index.css"> -->
+    <script src="https://kit.fontawesome.com/55fa51d470.js" crossorigin="anonymous"></script> 
+</head>
+<body>
+
+    <?php
+    require 'header.php';
+    ?>
+
+    <section>
+        <div class="container">
+            <h1 class="text-center pt-4">About Us</h1>
+            <div class="pt-5">
+
+            <div class="clearfix d-flex flex-column flex-lg-row justify-content-center align-items-center">
+            <img class="order-1 order-lg-1 me-lg-5 mb-4 img-fluid" src="aboutimg.jpg" alt="about image" width="500" style="box-shadow: 3px 3px 25px rgb(57, 57, 57);">
+            <p class="order-2 order-lg-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum accusamus nobis dolor soluta ipsum. Facilis, quis. Corrupti, doloribus veniam in illum perferendis distinctio modi officiis illo id facere, odit exercitationem atque ipsum vero delectus qui adipisci provident beatae laboriosam. Laborum et delectus ad. Iusto vel dolorem aperiam officia beatae, dicta itaque repellat sed accusantium cum dolorum iure cumque ut omnis ipsum commodi. Molestias dolore atque error sunt nisi blanditiis facere?</p>
+            </div>
+
+            <div  class="clearfix pt-5 d-flex flex-column flex-lg-row justify-content-center align-items-center">
+                <img class="order-1 order-lg-2 ms-lg-5 mb-4 img-fluid" src="aboutimg4.jpg" alt="about image" width="500" style="box-shadow: 3px 3px 25px rgb(57, 57, 57);">
+                <p class="order-2 order-lg-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, possimus! Magni nostrum, illum obcaecati, dolores possimus fuga praesentium natus quae blanditiis adipisci sequi laborum aliquid eos rem expedita. Debitis aliquid architecto quo! Molestiae incidunt iure ut explicabo eum voluptates, alias quod ducimus aliquid aspernatur neque sit harum porro earum. Quas aut vero quia corrupti suscipit nesciunt, porro minima? Fuga veritatis voluptatum suscipit quod iusto quae doloribus commodi dolor modi perferendis.</p>
+            </div>
+
+            <div class="clearfix pt-5 d-flex flex-column flex-lg-row justify-content-center align-items-center">
+                <img class="order-1 order-lg-1 me-lg-5 mb-4 img-fluid" src="aboutimg1.jpg" alt="about image" width="500" style="box-shadow: 3px 3px 25px rgb(57, 57, 57);">
+                <p class="order-2 order-lg-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia provident blanditiis, illo magni saepe omnis officiis sapiente vitae sequi! Exercitationem aperiam provident, suscipit repellendus quaerat vel quasi vitae, dolore culpa sed doloremque officiis, nulla accusantium modi sit voluptates reprehenderit ipsam maiores necessitatibus. Quis, nesciunt soluta debitis quae pariatur sapiente minus cumque omnis fugit sint harum est eos quia? Soluta architecto delectus incidunt quibusdam labore, voluptatum sequi fugiat consectetur inventore enim.</p>
+            </div>
+
+            <div class="clearfix pt-5 d-flex flex-column flex-lg-row justify-content-center align-items-center">
+                <img class="order-1 order-lg-2 ms-5 mb-4 img-fluid" src="aboutimg2.jpg" alt="about image" width="500" style="box-shadow: 3px 3px 25px rgb(57, 57, 57);">
+                <p class="order-2 order-lg-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quae debitis non eveniet distinctio sunt reiciendis modi? Dolorum, optio impedit atque sapiente error enim non dolores neque ullam assumenda numquam, dignissimos rerum voluptatum quis? Quis a reprehenderit necessitatibus ad, laborum autem facere hic nam tempora ipsam cumque nesciunt vitae corrupti rem, doloremque natus accusamus optio. Veritatis ut officiis omnis repudiandae sint earum eligendi, ad dignissimos qui temporibus doloribus quaerat non!</p>
+            </div>
+
+            <div class="clearfix pt-5 d-flex flex-column flex-lg-row justify-content-center align-items-center">
+                <img class="order-1 order-lg-1 me-5 mb-4 img-fluid " src="aboutimg3.jpg" alt="about image" width="500" style="box-shadow: 3px 3px 25px rgb(57, 57, 57);">
+                <p class="order-2 order-lg-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt a accusantium saepe? Maiores dignissimos debitis ex minima praesentium perferendis nobis, eligendi dolorum quasi ipsa recusandae, minus magnam reprehenderit repellat dolorem consectetur magni quis sequi. Odio exercitationem repellendus voluptatum at ad. Eos officia totam veniam autem, culpa veritatis fugit saepe sint voluptas aut aliquam quo distinctio non rem deleniti impedit temporibus pariatur reprehenderit corporis soluta amet assumenda aperiam, labore possimus. Error.</p>
+            </div>
+
+            </div>
+        </div>
+    </section>
+ 
+     <?php
+     require 'footer.php'
+     ?>
+
+    
+     <script src="index.js"></script>
+       
+
+</body>
+</html>
+
